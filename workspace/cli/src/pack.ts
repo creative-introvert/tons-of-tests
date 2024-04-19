76a29d85418d0e3650b4bff03e3a206dee77b2a2
Timm Stelzer <timmstelzer@gmail.com>
2024-04-19 17:34:20 +0200

    feat: begin adding cli

---
+++ workspace/cli/src/pack.ts
@@ -0,0 +1,58 @@
+import * as fs from 'node:fs/promises';
+import * as path from 'node:path';
+import {fileURLToPath} from 'node:url';
+
+import * as S from '@effect/schema/Schema';
+
+const PackageJsonSchema = S.parseJson(
+    S.Struct({
+        dependencies: S.Record(S.String, S.String),
+        license: S.String.pipe(S.nonEmpty()),
+        name: S.String.pipe(S.nonEmpty()),
+        repository: S.Record(S.String, S.String),
+        sideEffects: S.Array(S.String),
+        tags: S.Array(S.String),
+        version: S.String.pipe(S.nonEmpty()),
+    }),
+);
+
+const getPackageJson = async () => {
+    const workspacePath = path.join(
+        path.dirname(fileURLToPath(import.meta.url)),
+        '..',
+    );
+    const distPath = path.join(workspacePath, 'dist');
+    const buildPath = path.join(workspacePath, 'build');
+    const pjsonPath = path.join(workspacePath, 'package.json');
+
+    await fs.mkdir(distPath, {recursive: true});
+    const pjson = S.decodeSync(PackageJsonSchema)(
+        await fs.readFile(pjsonPath, {encoding: 'utf-8'}),
+    );
+
+    const next = Object.assign(pjson, {
+        files: ['dist/**/*'],
+        main: './dist/cjs/index.js',
+        module: './dist/esm/index.js',
+        types: './dist/dts/index.d.ts',
+        exports: {
+            '.': {
+                types: './dist/dts/index.d.ts',
+                import: './dist/esm/index.js',
+                default: './dist/cjs/index.js',
+            },
+        },
+        publishConfig: {access: 'public'},
+    });
+
+    await fs.writeFile(
+        path.join(distPath, 'package.json'),
+        JSON.stringify(next, null, 4),
+    );
+    console.log('created package.json');
+
+    await fs.cp(buildPath, path.join(distPath, 'dist'), {recursive: true});
+    console.log('copied build');
+};
+
+void getPackageJson();
