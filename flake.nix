57b316c1db2c6271599813eb496fc99cfb69a6cb
Timm Stelzer <timmstelzer@gmail.com>
2026-04-24 12:01:04 +0200

    chore: update deps

--- flake.nix
+++ flake.nix
@@ -1,44 +1,37 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
   outputs =
     {
       self,
       nixpkgs,
-      nixpkgs-unstable,
       flake-utils,
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        stable = nixpkgs.legacyPackages.${system};
-        unstable = nixpkgs-unstable.legacyPackages.${system};
-        nodejs = stable.nodejs_22;
-        corepackEnable = stable.runCommand "corepack-enable" { } ''
-          mkdir -p $out/bin
-          ${nodejs}/bin/corepack enable --install-directory $out/bin
-        '';
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            allowUnfree = true;
+          };
+        };
       in
+      with pkgs;
       {
-        devShells = {
-          default =
-            with stable;
-            mkShell {
-              BIOME_BINARY = "${unstable.biome}/bin/biome";
-              buildInputs = [
-                # see https://github.com/biomejs/biome-vscode/issues/295
-                # nixos can't deal with statically linked binaries, so
-                # we need to use the biome nix package
-                unstable.biome
-                nodejs
-                sqlite
-                corepackEnable
-              ];
-            };
+        devShells.default = mkShell {
+          BIOME_BINARY = "${biome}/bin/biome";
+          buildInputs = [
+            nodejs_24
+            pnpm
+            # see https://github.com/biomejs/biome-vscode/issues/295
+            # nixos can't deal with statically linked binaries, so
+            # we need to use the biome nix package
+            biome
+          ];
         };
       }
     );
